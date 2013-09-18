1120876d14a968da1442b6a2aa6597dc55b139db
Beldur <alexluecking@gmail.com>
2013-09-18 23:25:38 +0200

    Added Tests for sgf.go

--- sgf_test.go
+++ sgf_test.go
@@ -3,32 +3,49 @@ package libaduk
 import (
     "testing"
     "io/ioutil"
-    "fmt"
 )
 
 const (
     Testgame9x9 = "testing/Batora-okao.sgf"
     TestgameMultipleNodes = "testing/Multiple-Nodes.sgf"
     TestgameSmall = "testing/Small.sgf"
+    TestgameSmallMalformed = "testing/SmallMalformed.sgf"
 )
 
-func TestSgfRead(t *testing.T) {
+/**
+ * Small.sgf has this structure:
+ *  
+ *  X -> 1 -> 2 -> 3 -> 4 -> 5 -> 6
+ *             \-> 3 -> 4
+ *             |    \-> 4
+ *             \-> 3 -> 4
+ *
+ * Checks if 2 has 3 children and 3.1 has 2 children
+ */
+func TestSgfReadAndNumChildren(t *testing.T) {
     sgfData, _ := ioutil.ReadFile(TestgameSmall)
-    fmt.Println(string(sgfData))
-
     cursor, _ := NewCursor(sgfData)
+    root := cursor.tree.Next
 
-    tree := cursor.tree
+    if root.Next.numChildren != 1 {
+        t.Errorf("Node 1 should have 1 children but was: %+v", root.Next)
+    }
 
-    for tree != nil {
-        fmt.Printf("%p, %+v\n", tree, tree)
+    if root.Next.Next.numChildren != 3 {
+        t.Errorf("Node 2 should have 3 Children but was: %+v", root.Next.Next.Next)
+    }
 
-        down := tree.Down
-        for down != nil {
-            fmt.Printf("\t%p, %+v\n", down, down)
-            down = down.Down
-        }
+    if root.Next.Next.Next.Down.numChildren != 2 {
+        t.Errorf("Node 3.1 should have 2 children but was: %+v", root.Next.Next.Next.Down)
+    }
+}
 
-        tree = tree.Next
+// Tests if sgf is correctly found malformed (it has a not closing SEQUENCE_START character)
+func TestSgfReadMalformed(t *testing.T) {
+    sgfData, _ := ioutil.ReadFile(TestgameSmallMalformed)
+    _, err := NewCursor(sgfData)
+
+    if err == nil {
+        t.Errorf("Sgf should be malformed but was accepted!")
     }
 }
